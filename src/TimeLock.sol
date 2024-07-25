eab27e9432dcb29993d107ac4044b0a6b100bfc7
TanishqDsharma <tanishqsharma612@gmail.com>
2024-07-25 11:24:20 +0000

    Created TimeLock contract

---
+++ src/TimeLock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {TimelockController} from "lib/openzeppelin-contracts/contracts/governance/TimelockController.sol";
+
+
+contract TimeLock is TimelockController{
+
+}
