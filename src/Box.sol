08db08c974e41191919a467e1027042f8a35ef98
TanishqDsharma <tanishqsharma612@gmail.com>
2024-07-25 08:20:08 +0000

    Box Contract Implementation done

--- src/Box.sol
+++ src/Box.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 
 pragma solidity ^0.8.18;
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+contract Box is Ownable {
 
-contract Box {
+    uint256 private s_number;
+
+    event NumberChanged(uint256 number);
+
+    constructor() Ownable(msg.sender){}
+
+    // This function is only callable by the DAO
+    function store(uint256 newNumber) public onlyOwner{
+        s_number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() external view returns(uint256){
+        return s_number;
+    }
 
 }
